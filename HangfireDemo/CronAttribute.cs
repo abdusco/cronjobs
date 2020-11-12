b7a1441c3efd7645977c2ea9bb25e6a87e64056b
Abdussamet Kocak <sssmmt@gmail.com>
2020-11-12 19:16:06 +0300

    Add basic validation for cron expressions

--- HangfireDemo/CronAttribute.cs
+++ HangfireDemo/CronAttribute.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace HangfireDemo
 {
@@ -7,10 +7,17 @@ namespace HangfireDemo
     public class CronAttribute : Attribute
     {
         public string[] CronExpressions { get; }
-        
+
         public CronAttribute(params string[] cronExpressions)
         {
+            if (cronExpressions.Any(e => !IsValid(e)))
+            {
+                throw new ArgumentException("Invalid cron expression", nameof(cronExpressions));
+            }
+
             CronExpressions = cronExpressions;
         }
+
+        private bool IsValid(string cron) => cron.Split(" ").Length == 5;
     }
 }
