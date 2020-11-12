23d2e3a1fadaefc73f86cf8aeb6c57c86c0f6ab3
Abdussamet Kocak <sssmmt@gmail.com>
2020-11-12 18:33:41 +0300

    Remove support for triggerable controller actions

--- HangfireDemo/CronAttribute.cs
+++ HangfireDemo/CronAttribute.cs
@@ -3,14 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HangfireDemo
 {
-    [AttributeUsage(AttributeTargets.Method)]
-    public class TriggerableAttribute : HttpPostAttribute
-    {
-        public TriggerableAttribute(string name) : base(name)
-        {
-        }
-    }
-
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
     public class CronAttribute : Attribute
     {
